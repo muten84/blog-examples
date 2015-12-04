44a62fe6304912bc3bd7a32c740e23fcad3527ed
Luigi Bifulco <bifulcoluigi@gmail.com>
2015-12-04 14:54:25 +0100

    auto indent
    auto indent

--- javascript/webcomponents/xtag/app/xclock.js
+++ javascript/webcomponents/xtag/app/xclock.js
@@ -1,28 +1,28 @@
 define(['x-tag-core'], function(){
   console.log("Xtag loaded: "+xtag);
   xtag.register('x-clock', {
-  lifecycle: {
-    created: function(){
-      this.start();
-    }
-  },
-  methods: {
-    start: function(){
-      this.update();
-      this.xtag.interval = setInterval(this.update.bind(this), 1000);
+    lifecycle: {
+      created: function(){
+        this.start();
+      }
     },
-    stop: function(){
-      this.xtag.interval = clearInterval(this.xtag.interval);
+    methods: {
+      start: function(){
+        this.update();
+        this.xtag.interval = setInterval(this.update.bind(this), 1000);
+      },
+      stop: function(){
+        this.xtag.interval = clearInterval(this.xtag.interval);
+      },
+      update: function(){
+        this.textContent = new Date().toLocaleTimeString();
+      }
     },
-    update: function(){
-      this.textContent = new Date().toLocaleTimeString();
+    events: {
+      tap: function(){
+        if (this.xtag.interval) this.stop();
+        else this.start();
+      }
     }
-  },
-  events: {
-    tap: function(){
-      if (this.xtag.interval) this.stop();
-      else this.start();
-    }
-  }
   });
 });
