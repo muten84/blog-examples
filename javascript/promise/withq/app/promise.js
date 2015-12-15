58df4982889a7f76969072e9058b8b30690ca3bc
Luigi Bifulco <bifulcoluigi@gmail.com>
2015-12-15 17:59:32 +0100

    promise example
    promise example

--- javascript/promise/withq/app/promise.js
+++ javascript/promise/withq/app/promise.js
@@ -1,15 +1,14 @@
 define(['q'], function(Q){
-  console.log("Q loaded: "+Q);
-  Q.fcall(function () {
+  var example = {};
+  var fcall = Q.fcall(function () {
     console.log("invoked")
     return [{ foo: "bar" }, { foo: "baz" }];
   })
   .then(function (value) {
     console.log("processing values: "+value)
     return value[0].foo;
-  })
-  .all();
-
-  
-  //Q.all(promise);
+  });
+  example.fcall = fcall;
+  return example;
+  //.all();
 })
--- javascript/promise/withq/config.js
+++ javascript/promise/withq/config.js
@@ -14,22 +14,27 @@ require.config({
 
 //require our angularApp for bootstrapping it when DOM ready function was called!
 require(
-	['app/promise'],	function() {
+	['app/promise'],	function(example) {
 		'use strict';
-		console.log("Application loaded.... ");
+		console.log("Application loaded.... "+example);
+		/*example fcall usage*/
+		example.fcall.all();
 	});
 
-	require(['app/promiseXHR'],	function(example) {
-		'use strict';
-		example.get("http://localhost:1337/camera/camera_control.cgi?loginuse=&loginpas=")
-		.then(function (responseText) {
-			// If the HTTP response returns 200 OK, log the response text.
-			console.log(responseText);
-		}, function (error) {
-			// If there's an error or a non-200 status code, log the error.
-			console.error(error);
-		}, function (progress) {
-			// Log the progress as it comes in.
-			console.log("Request progress: " + Math.round(progress * 100) + "%");
-		});
+require(['app/promiseXHR'],	function(xhr) {
+	'use strict';
+	/*example custom XHR promised usage*/
+	xhr.get("http://localhost:1337/camera/camera_control.cgi?loginuse=&loginpas=")
+	/*then take in input the resolve the reject and the notify function to be updated
+	of the XHR call*/
+	.then(function (responseText) {
+		// If the HTTP response returns 200 OK, log the response text.
+		console.log(responseText);
+	}, function (error) {
+		// If there's an error or a non-200 status code, log the error.
+		console.error(error);
+	}, function (progress) {
+		// Log the progress as it comes in.
+		console.log("Request progress: " + Math.round(progress * 100) + "%");
 	});
+});
