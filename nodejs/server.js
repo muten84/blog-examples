ea974bcb19bc43e2002f26226787688a4c0417e3
Luigi Bifulco <bifulcoluigi@gmail.com>
2015-12-22 17:57:42 +0100

    server js
    express server in javascript

--- nodejs/server.js
+++ nodejs/server.js
@@ -1,9 +1,26 @@
-/*Http server with node.js
-Launch with node server.js*/
-var http = require('http');
+var express = require("express"),
+    app = express(),
+    bodyParser = require('body-parser'),
+    errorHandler = require('errorhandler'),
+    methodOverride = require('method-override'),
+    hostname = process.env.HOSTNAME || 'localhost',
+    port = parseInt(process.env.PORT, 10) || 4567,
+    publicDir = process.argv[2] || __dirname + '/public';
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
+app.get("/", function (req, res) {
+  res.redirect("/index.html");
 });
-server.listen(8888);
+
+app.use(methodOverride());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static(publicDir));
+app.use(errorHandler({
+  dumpExceptions: true,
+  showStack: true
+}));
+
+console.log("Simple static server showing %s listening at http://%s:%s", publicDir, hostname, port);
+app.listen(port, hostname);
